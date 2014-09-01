349085d4b702073fe5d36a2316e44c4611de807a
JorgeArino <jarino@robotnik.es>
2014-09-01 18:56:33 +0200

    back_and_forth_movement.cpp updated but not tested

--- src/back_and_forth_movement.cpp
+++ src/back_and_forth_movement.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 class BackAndForthMovement{
 	public:
-		float pos_limit; // position limit (rad)
-		float w_limit; // angular velocity limit (rad/s)
+		float pos_limit_; // position limit (rad)
+		float w_limit_; // angular velocity limit (rad/s)
 		
 		sensor_msgs::JointState joint_command_; // joint to send
 		sensor_msgs::JointState joint_state_;   // joint to receive
@@ -18,8 +18,10 @@ class BackAndForthMovement{
 		ros::Publisher joint_pub_;
 		ros::Subscriber joint_sub_;
 		int state_;
-		float desired_freq_;
+		double desired_freq_;
 		string name_;
+		bool joint_received_;
+		ros::Rate *loop_rate_;
 		
 		
 		
@@ -27,29 +29,47 @@ class BackAndForthMovement{
 		{
 			ROS_INFO("back and forth movement started");
 			
-			pos_limit = PI / 2;
-			w_limit = 0.1;
+			pos_limit_ = PI / 2;
+			w_limit_ = 0.1;
 			state_ = 1;
-			desired_freq_ = 30.0;
+			desired_freq_ = 100.0;
 			name_ = "H42";
+			joint_received_ = false;
+			loop_rate_ = new ros::Rate(desired_freq_);
 			
 			joint_pub_ = nh.advertise<sensor_msgs::JointState>("/joint_commands",1);
 			joint_sub_ = nh.subscribe<sensor_msgs::JointState>("/joint_states", 1, &BackAndForthMovement::jointStateCallback, this);
-			
+
 			controlLoop();
 		}
 		
+		
 		void controlLoop()
 		{
-			ros::Rate r(desired_freq_);
-			while(ros::ok)
+			//ros::Rate loop_rate_(30);
+			//loop_rate_.reset();
+			while(ros::ok())
 			{
-				if(state_==1)
-					initialize();
+				ROS_INFO("debug"); 
+				// only move the joint when a joint_state msg is received
+				if(joint_received_)
+				{
 					
+					if(state_ == 1)
+						initialize();
+					else if(state_ == 2)
+						turnLeft();
+					else if(state_ == 3)
+						turnRight();
+				}
+				joint_received_ = false;
+				
+				
 				ros::spinOnce();
-				r.sleep();
+				loop_rate_->sleep();
+				
 			}
+			ros::shutdown();
 		}
 		
 		/*!
@@ -57,34 +77,71 @@ class BackAndForthMovement{
 		 */
 		void initialize()
 		{
+			ROS_INFO("Initializing joint"); 
+			
 			// if not centered
 			if(joint_state_.position[0] != 0.0)
 			{
-				sensor_msgs::JointState joint_center;
-				joint_center.name[0] = name_;
-				joint_center.position[0] = 0.0;
+				
+				sensor_msgs::JointState center_joint;
+				center_joint.name[0] = name_;
+				center_joint.position[0] = 0.0;
 				// @todo: set velocity depending the direction it has to move
-				joint_pub_.publish(joint_center);
+				joint_pub_.publish(center_joint);
 			}
 			// if centered
 			else
 			{
-				turnLeft();
+				state_ = 2; // turn left
 			}
 		}
 		
 		void turnLeft()
 		{
+			ROS_INFO("Turning left");
 			
+			// limit not reached
+			if(joint_state_.position[0] < pos_limit_)
+			{
+				sensor_msgs::JointState left_joint;
+				left_joint.name[0] = name_;
+				left_joint.position[0] = pos_limit_;
+				left_joint.velocity[0] = w_limit_;
+				joint_pub_.publish(left_joint);
+			}
+			// limit reached
+			else 
+			{
+				state_ = 3; // turn right
+			}
 		}
 		
 		void turnRight()
 		{
+			ROS_INFO("Turning right");
+			
+			// limit not reached
+			if(joint_state_.position[0] > -pos_limit_)
+			{
+				sensor_msgs::JointState right_joint;
+				right_joint.name[0] = name_;
+				right_joint.position[0] = -pos_limit_;
+				right_joint.velocity[0] = -w_limit_;
+				joint_pub_.publish(right_joint);
+			}
+			// limit reached
+			else 
+			{
+				state_ = 3; // turn right
+			}
 			
 		}
 		
 		void jointStateCallback(const sensor_msgs::JointState joint_state)
 		{
+			ROS_INFO("joint callback");
+			joint_received_ = true;
+			
 			if(joint_state.name.size() == 0)
 				ROS_WARN("Joint state received is empty");
 			else if(joint_state.name.size() > 1)
@@ -106,9 +163,6 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "back_and_forth_movement");
 	BackAndForthMovement backAndForthMovement;
 		
-	//while(ros::ok)
-	//{
-			//ros::spinOnce();
-			
-	//}
+	return 0;
+
 }
